9435d3ef75fa2f84f0474e8dd2c8fba87527fd9a
Dan Padgett <dumbledore3@gmail.com>
2026-04-24 15:29:45 -0700

    add BTWD 2026 landing page.

--- render.js
+++ render.js
@@ -1,8 +1,8 @@
 const SSI = require('ssi');
 const path = require('path');
 
-const inputDir = path.join(__dirname, 'src');
-const outputDir = path.join(__dirname, 'dist');
+const inputDir = path.join(__dirname, 'src').replace(/\\/g, '/');
+const outputDir = path.join(__dirname, 'dist').replace(/\\/g, '/');
 
 const compiler = new SSI(inputDir, outputDir, '/**/*.html');
 compiler.compile();
